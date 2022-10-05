952c749207bded1e34431edb96576b9e5fca1fb3
Aviral Mehrotra <aviral.mehrotra@gmail.com>
2022-10-04 19:56:23 -0500

    Add files via upload

---
+++ PASC-test/PASC-test/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  PASC-test
+//
+//  Created by Aviral Mehrotra on 9/28/22.
+//
+
+import SwiftUI
+import Foundation
+
+struct ContentView: View {
+    
+    @State private var backgroundColor = Color.green.opacity(0.15)
+    @State var clicks = 0
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            ZStack {
+                
+                
+                VStack(spacing: 50) {
+                    
+                    Spacer()
+                    
+                    Text("Click counter: \(clicks)")
+                        .font(.title)
+                        .navigationTitle("PASC Test App")
+                    
+                    
+                    switch clicks {
+                    case 0:
+                        Text("Click to increase the counter")
+                    case 10 ..< 25:
+                        Text("Good start!")
+                    case 25 ..< 50:
+                        Text("Great job!")
+                    case 50 ..< 100:
+                        Text("Wow!")
+                    case 100 ... Int.max:
+                        Text("Impressive!")
+                    default:
+                        Text("")
+                    }
+                    
+                    HStack(spacing: 50) {
+                        Button(action: {
+                            self.clicks += 1
+                        }) {
+                            Text("Increase click count")
+                        }
+                        
+                        Button(action: {
+                            self.clicks = 0
+                        }) {
+                            Text("Reset")
+                        }
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    ColorPicker("Change background color", selection: $backgroundColor)
+                        .padding()
+                        .padding(.bottom, 30)
+                }
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(backgroundColor)
+            .ignoresSafeArea()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PASC-test/PASC-test/PASC_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  PASC_testApp.swift
+//  PASC-test
+//
+//  Created by Aviral Mehrotra on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct PASC_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
